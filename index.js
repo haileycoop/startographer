9bb0b7e5a7f20287c4d562d80d6f5b18b1d2d422
haileycoop <haileycooperrider@gmail.com>
2020-12-22 00:13:16 +1100

    updated index.js to include ability to fetch messages when a user types 'fetch' in any channel. Right now the messages are just printed to the console log.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const Discord = require('discord.js');
  const client = new Discord.Client();
 require('dotenv').config()
+const fetchAll = require('discord-fetch-all');
+
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
-client.on('message', msg => {
+client.on('message', async msg => {
  if (msg.content === 'ping') {
- msg.reply('Feeling fancy');
- }
+ 	msg.reply('pong');
+ 	} else if (msg.content === 'fetch') {
+	 	const allMessages = await fetchAll.messages(msg.channel, {
+		    reverseArray: true, // Reverse the returned array
+		    userOnly: true, // Only return messages by users
+		    botOnly: false, // Only return messages by bots
+		    pinnedOnly: false, // Only returned pinned messages
+	});
+
+	console.log(allMessages);
+ 	}
  });
 
 client.login(process.env.TOKEN);

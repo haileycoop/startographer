2fd49facd74030f68cf167ad9f452e1db395d7b4
haileycoop <haileycooperrider@gmail.com>
2020-12-23 14:20:27 +1100

    added very detailed comments

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+//says: we're using discord.js library
 const Discord = require('discord.js');
+
+//? says: this program runs a bot (client), so lets use the discord.client class
  const client = new Discord.Client();
+
+//looks for the separate .env config file, which stores the token
 require('dotenv').config()
+
+//says: we're using the 'discord-fetch-all' package
 const fetchAll = require('discord-fetch-all');
 
-
-client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
+//? I think this is an 'even't which I need to learn about which
+client.on('ready', () => { //? this checks whether the bot is connected to the server - not sure where 'ready' comes from
+ console.log(`Logged in as ${client.user.tag}!`); //? if the connection is good, it prints this message to the console, using the bot name 
  });
 
-client.on('message', async msg => {
- if (msg.content === 'ping') {
- 	msg.reply('Feeling upgraded');
- 	} else if (msg.content === 'fetch') {
-	 	const allMessages = await fetchAll.messages(msg.channel, {
+//? listens to every message, scanning for if it meets the conditions in the if statements
+client.on('message', async msg => { //'async' was added to support the fetch/await line. Not sure how the => structure works
+ if (msg.content === 'ping') { //if any user sends a message that is exactly this string
+ 	msg.reply('Feeling upgraded'); //bot responds with this string
+ 	} else if (msg.content === 'fetch') { //if a user types this string inany channel...
+	 	const allMessages = await fetchAll.messages(msg.channel, { //go get all the messages in that channel
 		    reverseArray: true, // Reverse the returned array
 		    userOnly: true, // Only return messages by users
 		    botOnly: false, // Only return messages by bots
 		    pinnedOnly: false, // Only returned pinned messages
 	});
 
-	console.log(allMessages);
+	console.log(allMessages); //takes all the fetched messages including metadata and dumps them into the console as an array
  	}
  });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN); //logs the bot into the server using the token stored in the .env file

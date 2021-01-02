7460b480258c7ec04b6469bc17718834ac7b0851
haileycoop <haileycooperrider@gmail.com>
2021-01-02 21:35:21 +1100

    now able to fetch all messages objects and push only message content into a new array + new message.json file added for reference

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //says: we're using discord.js library
 const Discord = require('discord.js');
 
-//? says: this program runs a bot (client), so lets use the discord.client class
- const client = new Discord.Client();
+// says: this program is a bot (client), so make an instance of the Discord.Client class
+const client = new Discord.Client();
 
 //looks for the separate .env config file, which stores the token
 require('dotenv').config()
@@ -12,23 +12,26 @@ const fetchAll = require('discord-fetch-all');
 
 //? I think this is an 'even't which I need to learn about which
 client.on('ready', () => { //? this checks whether the bot is connected to the server - not sure where 'ready' comes from
- console.log(`Logged in as ${client.user.tag}!`); //? if the connection is good, it prints this message to the console, using the bot name 
- });
+  console.log(`Logged in as ${client.user.tag}!`); //? if the connection is good, it prints this message to the console, using the bot name 
+});
 
 //? listens to every message, scanning for if it meets the conditions in the if statements
 client.on('message', async msg => { //'async' was added to support the fetch/await line. Not sure how the => structure works
- if (msg.content === 'ping') { //if any user sends a message that is exactly this string
- 	msg.reply('Feeling upgraded'); //bot responds with this string
- 	} else if (msg.content === 'fetch') { //if a user types this string inany channel...
-	 	const allMessages = await fetchAll.messages(msg.channel, { //go get all the messages in that channel
-		    reverseArray: true, // Reverse the returned array
-		    userOnly: true, // Only return messages by users
-		    botOnly: false, // Only return messages by bots
-		    pinnedOnly: false, // Only returned pinned messages
-	});
-
-	console.log(allMessages); //takes all the fetched messages including metadata and dumps them into the console as an array
- 	}
- });
+  if (msg.content === 'ping') { //if any user sends a message that is exactly this string
+    msg.reply('Feeling fancy'); //bot responds with this string
+  } else if (msg.content === 'fetch') { //if a user types this string inany channel...
+    const allMessages = await fetchAll.messages(msg.channel, { //go get all the messages in that channel
+      reverseArray: true, // Reverse the returned array
+      userOnly: true, // Only return messages by users
+      botOnly: false, // Only return messages by bots
+      pinnedOnly: false, // Only returned pinned messages
+    });
+    let starBucket = [];
+    for (let eachMessage of allMessages) {
+      starBucket.push(eachMessage.content);
+    }
+		console.log(starBucket); //prints that array in the console
+  }
+});
 
 client.login(process.env.TOKEN); //logs the bot into the server using the token stored in the .env file

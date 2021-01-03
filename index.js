324eef97b747cc3a199e3e11456ba04e635ddc87
haileycoop <haileycooperrider@gmail.com>
2021-01-03 17:58:41 +1100

    major refactor of code but timestamp conversion still not working in CSV

--- index.js
+++ index.js
@@ -1,79 +1,62 @@
-//says: we're using discord.js library
-const Discord = require('discord.js');
+//dependencies
+const Discord = require('discord.js'); //Discord.js
+const fetchAll = require('discord-fetch-all'); //fetching all messages from a channel
+const ObjectsToCsv = require('objects-to-csv'); //dump an array of objects in a CSV
+require('dotenv').config() //looks for the separate .env config file, which stores the token
 
-// says: this program is a bot (client), so make an instance of the Discord.Client class
-const client = new Discord.Client();
 
-//looks for the separate .env config file, which stores the token
-require('dotenv').config()
-
-//says: we're using the 'discord-fetch-all' package
-const fetchAll = require('discord-fetch-all');
-
-//says: we're using the 'objects-to-csv' package
-const ObjectsToCsv = require('objects-to-csv');
-
-//? I think this is an 'even't which I need to learn about which
-client.on('ready', () => { //? this checks whether the bot is connected to the server - not sure where 'ready' comes from
-  console.log(`Logged in as ${client.user.tag}!`); //? if the connection is good, it prints this message to the console, using the bot name 
+//protocols for starting up the bot
+const client = new Discord.Client(); //creates an object that lets the bot connect to the Discord API
+client.login(process.env.TOKEN); //log the bot into the server using the token stored in the .env file
+client.on('ready', () => { //connect the bot to the server
+  console.log(`Logged in as ${client.user.tag}!`); //let us know the connection worked
 });
 
-//? listens to every message, scanning for if it meets the conditions in the if statements
-client.on('message', async msg => { //'async' was added to support the fetch/await line. Not sure how the => structure works
-  if (msg.content === 'ping') { //if any user sends a message that is exactly this string
-    msg.reply('Feeling fancy'); //bot responds with this string
-  } else if (msg.content === 'fetch') { //if a user types this string inany channel...
+//listens to every message, then delivers appropriate response
+client.on('message', msg => {  //the function 'msg' is called when a message is received
+  if (msg.author.bot) return;
+  if (msg.content === 'ping') {msg.reply("Feeling fancy!")}
+  else if (msg.content === 'fetch') {starMapper(msg)};
+});
+
+//separate function for fetching messages
+async function starMapper(msg) {
     const allMessages = await fetchAll.messages(msg.channel, { //go get all the messages in that channel
       reverseArray: true, // Reverse the returned array
       userOnly: true, // Only return messages by users
       botOnly: false, // Only return messages by bots
       pinnedOnly: false, // Only returned pinned messages
     });
-
-    // fetch only the timestamp and content of allMessages and create a new array called Starbucket
-    const starBucket = allMessages.map(({
+    const starBucket = allMessages.map(({ //trim the array to include only timestamp and content
       createdTimestamp,
       content
     }) => ({
       createdTimestamp,
       content
-    }));
-
-    //convert discord createdTimestamp to a preferred date format
-
-    function convertTimestamps() {
-      for (let star of starBucket) {
-        star.createdTimestamp = new Date(star.createdTimestamp);
-      }
-    }
-
-    convertTimestamps();
-
-    // Save CSV file to disk:
-
-    async function createCSV() {
-      const result = await convertTimestamps();
-      const csv = new ObjectsToCsv(starBucket);
-      csv.toDisk('./starmap.csv');
-
-
-    }
-
-    createCSV();
-
-    async function sendCSV() {
-      const attachment = new Discord.MessageAttachment('starmap.csv');
-      // Send the attachment in the message channel
-      msg.channel.send(`Here is your starmap, explorer.`, attachment);
-    }
-
-    sendCSV();
-
-    //print the output to the console, for testing
-
-    console.log(starBucket);
-
+    })); 
+    await convertTimestamps(starBucket); //call timestamp conversion function
+    await createCSV(starBucket); //call the function that turns the array into a CSV
+    await sendCSV(msg); //call the function that attaches and sends the CSV
+    const consolecheck = console.log(starBucket); //print the output to the console, for testing
   }
-});
 
-client.login(process.env.TOKEN); //logs the bot into the server using the token stored in the .env file
+
+//function to convert discord createdTimestamp to a preferred date format
+async function convertTimestamps(starBucket) {
+  for (let star of starBucket) {
+    star.createdTimestamp = new Date(star.createdTimestamp);
+  }
+}
+
+//function to save CSV file to disk:
+async function createCSV(starBucket) {
+  const csv = new ObjectsToCsv(starBucket);
+  csv.toDisk('./starmap.csv');
+}
+
+//function to attach and send the CSV
+async function sendCSV(msg){
+  const CSVattachment = new Discord.MessageAttachment('starmap.csv');
+  const sendmap = msg.channel.send(`Here is your starmap, explorer.`, CSVattachment);
+};
+
